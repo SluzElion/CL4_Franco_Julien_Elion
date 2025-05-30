efd4b8a4bd0be8045013ae3328244543a9d33fb9
SluzElion <Elion_Berisha@sluz.ch>
2025-05-30 10:47:46 +0200

    Delete js directory

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger');
-  const navLinks = document.querySelector('.nav-links');
-
-  burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
-});
